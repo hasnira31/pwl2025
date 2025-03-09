2be6731b838263f2a52ef20c1dd8f19913a28b5d
Rofi Dwi Firezeki <40338299-rofidwifirezeki@users.noreply.replit.com>
2025-03-09 15:27:42 +0000

    Restored to '1cd294b815391c3863e37e8b3b4619888313b668'
    Replit-Restored-To: 1cd294b815391c3863e37e8b3b4619888313b668

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +16,3 @@ use App\Http\Controllers\EmployeeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('employees', EmployeeController::class);
